093917c71ae4a1e619457b1ed23eb20b298dc001
lavindu perera <lavi156perera@gmail.com>
2025-11-28 19:10:05 +0530

    completed the basic functionality for calculating probability

--- hits_or_miss.py
+++ hits_or_miss.py
@@ -1,12 +1,14 @@
 def hit_miss(x, y , N):
     radius = (x**2 + y**2)**0.5
     hittinG_count = 0
+    missing_count = 0
     if radius <= 1:
         hittinG_count += 1
-        print("Hitting inside the dartboard")
+        
     else:
-        print("Missing the dartboard")
+        missing_count += 1
+       
     
-    return hittinG_count
+    return hittinG_count, missing_count
     
     
--- main.py
+++ main.py
@@ -3,18 +3,33 @@ from hitting_probability import hitting_prob
 import random
 
 def main():
-    # Number of dart throws
-    N = int(input("Enter the number of dart throws: "))
-    hittinG_count = 0
-    for _ in range(N):
-        x = random.uniform(-2, 2)
-        y = random.uniform(-2, 2)
-        
-        hittinG_count += hit_miss(x, y, N)
-        print(f"hittinG_count: {hittinG_count}")
+    while True:
+        # Number of dart throws
+        try:
+            N = int(input("Enter the number of dart throws: "))
+            if N > 0:
+                break
+            else:
+                print("Please enter a positive integer for the number of throws.")
+        except ValueError:
+            print("Invalid input. Please enter a positive integer.")
 
-        hitting_probability = hitting_prob(hittinG_count, N)
-        print(f"Hitting Probability after {_ + 1} throws: {hitting_probability}")
+    
+    total_hitting_count = 0
+    total_missing_count = 0
+    for _ in range(N):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        
+        hittinG_count , missing_count= hit_miss(x, y , N)
+        total_hitting_count += hittinG_count
+        total_missing_count += missing_count
+        
+    print(f"total hitting count: {total_hitting_count}")
+    print(f"total missing count: {total_missing_count}")
+
+    hitting_probability = hitting_prob(total_hitting_count, N)
+    print(f"Hitting Probability after {N} throws: {round(hitting_probability, 2)} %")
         
 
 if __name__ == "__main__":
