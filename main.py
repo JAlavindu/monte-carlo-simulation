6bb5664c22c8e4936b6a1f67272dfb94d4b84a64
lavindu perera <lavi156perera@gmail.com>
2025-11-28 17:38:05 +0530

    added randomness for the x, y coordinates

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from hits_or_miss import hit_miss
+import random
 
 def main():
-    x = float(input("Enter x coordinate: "))
-    y = float(input("Enter y coordinate: "))
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
     result  = hit_miss(x, y)
     print(result)
 
